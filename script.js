14896b4572f70f3fdcce183e0924dbb9ef9a1571
SiapaLupa <muhammadilyasafadhlih@gmail.com>
2023-05-10 16:47:38 +0700

    add delete functionality

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var _this = this;
 var todo = document.getElementById("todo");
 var submit = document.getElementById("submit");
 var table = document.getElementById("table");
@@ -8,59 +7,83 @@ var intervalId = undefined;
 var separator = "$$";
 var key = "todo";
 renderData(table);
-search.onkeyup = function (event) {
-    renderData(_this.table, event.target.value);
+search.onkeyup = (event) => {
+    renderData(this.table, event.target.value);
 };
-submit.onclick = function () {
+submit.onclick = () => {
     if (todo.value.length <= 0)
         return invalidInput();
-    var prevData = window.localStorage.getItem(key);
-    var data = typeof prevData !== "string"
+    let prevData = window.localStorage.getItem(key);
+    let data = typeof prevData !== "string"
         ? todo.value
         : [prevData, todo.value].join(separator);
     todo.value = "";
     insertInfo("Success!", "green");
     window.localStorage.setItem(key, data);
-    renderData(_this.table);
+    renderData(this.table);
 };
 function invalidInput() {
     clearTimeout(window.intervalId);
     insertInfo("Cannot add empty todo!");
 }
-function insertInfo(text, color) {
-    if (color === void 0) { color = "red"; }
-    var initialTextColor = "red";
+function insertInfo(text, color = "red") {
+    const initialTextColor = "red";
     info.innerText = text;
     info.style.setProperty("--text-color", color);
-    window.intervalId = setTimeout(function () {
+    window.intervalId = setTimeout(() => {
         info.innerText = "";
         info.style.setProperty("--text-color", initialTextColor);
     }, 1000);
 }
-function renderData(table, filter) {
-    if (filter === void 0) { filter = ""; }
-    var storageItem = window.localStorage.getItem(key);
+function renderData(table, filter = "") {
+    let storageItem = window.localStorage.getItem(key);
     if (typeof storageItem !== "string")
-        return;
-    var items = storageItem.split(separator);
+        return clearData(table);
+    let items = storageItem.split(separator);
     clearData(table);
-    items.filter(function (item) { return item.includes(filter); }).forEach(function (item, index) {
-        var tableRow = document.createElement("tr");
-        var number = document.createElement("td");
-        var todo = document.createElement("td");
-        var checkCell = document.createElement("td");
-        ;
-        var checklist = document.createElement("input");
+    items.filter((item) => item.includes(filter)).forEach((item, index) => {
+        let tableRow = document.createElement("tr");
+        let number = document.createElement("td");
+        let form = document.createElement("form");
+        let editTodo = document.createElement("input");
+        let submitEditTodo = document.createElement("input");
+        let todoCell = document.createElement("td");
+        let checkCell = document.createElement("td");
+        let trashCell = document.createElement("td");
+        let checklist = document.createElement("input");
+        let trash = document.createElement("button");
+        trash.innerText = "x";
+        trash.onclick = () => {
+            destroyData(items, index, table);
+        };
         checklist.type = "checkbox";
-        todo.innerText = item;
+        // trash.style.visibility = "hidden"
+        // checklist.style.visibility = "hidden"
+        submitEditTodo.type = "submit";
+        editTodo.value = item;
+        form.onsubmit = (e) => e.preventDefault();
         number.innerText = String(index + 1).concat(".");
+        form.appendChild(editTodo);
+        todoCell.appendChild(form);
         checkCell.appendChild(checklist);
+        trashCell.appendChild(trash);
         tableRow.appendChild(number);
-        tableRow.appendChild(todo);
+        tableRow.appendChild(todoCell);
         tableRow.appendChild(checkCell);
+        tableRow.appendChild(trashCell);
         table.appendChild(tableRow);
     });
 }
 function clearData(table) {
-    Array.from(table.children).forEach(function (child) { return child.remove(); });
+    Array.from(table.children).forEach((child) => child.remove());
+}
+function destroyData(items, index, table) {
+    if (items.length <= 1) {
+        window.localStorage.removeItem(key);
+        return renderData(table);
+    }
+    items.splice(index, 1);
+    let dataAfterDestroy = items.join(separator);
+    window.localStorage.setItem(key, dataAfterDestroy);
+    return renderData(table);
 }
--- script.ts
+++ script.ts
@@ -42,21 +42,60 @@ function insertInfo(text: string, color: string = "red"): void {
 
 function renderData(table: HTMLTableElement, filter: string = ""): void {
   let storageItem: string | null = window.localStorage.getItem(key);
-  if (typeof storageItem !== "string") return;
+  if (typeof storageItem !== "string") return clearData(table);
   let items: Array<string> = storageItem.split(separator);
   clearData(table);
-  items.filter((item) => item.includes(filter)).forEach((item: string, index: number) => {
-    let tableRow = document.createElement("tr") as HTMLTableRowElement;
-    let number = document.createElement("td") as HTMLTableCellElement;
-    let todo = document.createElement("td") as HTMLTableCellElement;
-    todo.innerText = item;
-    number.innerText = String(index + 1).concat(".");
-    tableRow.appendChild(number);
-    tableRow.appendChild(todo);
-    table.appendChild(tableRow);
-  });
+  items.filter((item) => item.includes(filter)).forEach(
+    (item: string, index: number) => {
+      let tableRow = document.createElement("tr") as HTMLTableRowElement;
+      let number = document.createElement("td") as HTMLTableCellElement;
+      let form = document.createElement("form") as HTMLFormElement;
+      let editTodo = document.createElement("input") as HTMLInputElement;
+      let submitEditTodo = document.createElement("input") as HTMLInputElement;
+      let todoCell = document.createElement("td") as HTMLTableCellElement;
+      let checkCell = document.createElement("td") as HTMLTableCellElement;
+      let trashCell = document.createElement("td") as HTMLTableCellElement;
+      let checklist = document.createElement("input") as HTMLInputElement;
+      let trash = document.createElement("button") as HTMLButtonElement;
+      trash.innerText = "x";
+      trash.onclick = () => {
+        destroyData(items, index, table);
+      };
+      checklist.type = "checkbox";
+      // trash.style.visibility = "hidden"
+      // checklist.style.visibility = "hidden"
+      submitEditTodo.type = "submit";
+      editTodo.value = item;
+      form.onsubmit = (e: Event) => e.preventDefault();
+      number.innerText = String(index + 1).concat(".");
+      form.appendChild(editTodo);
+      todoCell.appendChild(form);
+      checkCell.appendChild(checklist);
+      trashCell.appendChild(trash);
+      tableRow.appendChild(number);
+      tableRow.appendChild(todoCell);
+      tableRow.appendChild(checkCell);
+      tableRow.appendChild(trashCell);
+      table.appendChild(tableRow);
+    },
+  );
 }
 
 function clearData(table: HTMLTableElement): void {
   Array.from(table.children).forEach((child) => child.remove());
+}
+
+function destroyData(
+  items: Array<string>,
+  index: number,
+  table: HTMLTableElement,
+): void {
+  if (items.length <= 1) {
+    window.localStorage.removeItem(key);
+    return renderData(table);
+  }
+  items.splice(index, 1);
+  let dataAfterDestroy = items.join(separator);
+  window.localStorage.setItem(key, dataAfterDestroy);
+  return renderData(table);
 }
